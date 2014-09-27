9cb3c42d44a92aa7198bce8127e44563cf8627a9
Jerry Lee <oldratlee@gmail.com>
2014-09-27 23:38:56 +0800

    add testee class

---
+++ src/main/java/com/oldratlee/jvisualvm/Testee.java
@@ -0,0 +1,101 @@
+package com.oldratlee.jvisualvm;
+
+import java.util.Date;
+
+/**
+ * @author Jerry Lee
+ */
+public class Testee {
+    static long countTimeIn100ms = 0;
+
+    public static void main(String[] args) throws Exception {
+        countTimeIn100ms = guessCountTimeIn100ms();
+
+        Thread runAndSleepTask = new Thread(new RunAndSleepTask(), "RunAndSleepTask");
+        Thread runAndWaitTask = new Thread(new RunAndWaitTask(), "RunAndWaitTask");
+        Thread runAndLongSleepTask = new Thread(new RunAndLongSleepTask(), "RunAndLongSleepTask");
+
+        runAndSleepTask.start();
+        runAndWaitTask.start();
+        runAndLongSleepTask.start();
+
+        System.out.println("Tasks started!");
+        Thread.sleep(Long.MAX_VALUE);
+    }
+
+    public static class RunAndSleepTask implements Runnable {
+        @Override
+        public void run() {
+            String name = this.getClass().getSimpleName();
+            while (true) {
+                for (long i = 0; i < countTimeIn100ms; i++) {
+                }
+                System.out.println(name + ": " + new Date());
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public static class RunAndWaitTask implements Runnable {
+        @Override
+        public void run() {
+            String name = this.getClass().getSimpleName();
+            while (true) {
+                for (long i = 0; i < countTimeIn100ms; i++) {
+                }
+                try {
+                    System.out.println(name + ": " + new Date());
+                    synchronized (this) {
+                        this.wait(100);
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public static class RunAndLongSleepTask implements Runnable {
+        @Override
+        public void run() {
+            String name = this.getClass().getSimpleName();
+            while (true) {
+                for (long i = 0; i < countTimeIn100ms; i++) {
+                }
+                System.out.println(name + ": " + new Date());
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    /**
+     * @return 100ms可以做{@code long}的计数次数。
+     */
+    static long guessCountTimeIn100ms() {
+        System.out.println("start guess.");
+
+        long count = 1000L * 1000 * 1000;
+
+        // simple warm-up
+        for (long i = 0; i < count; i++) {
+            // nothing!
+        }
+
+        long tick = System.currentTimeMillis();
+        for (long i = 0; i < count; i++) {
+            // nothing!
+        }
+        long duration = System.currentTimeMillis() - tick;
+        System.out.printf("1G times count take %d ms.\n", duration);
+
+        return count * 100 / duration;
+    }
+}
